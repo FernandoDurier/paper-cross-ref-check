ba3ed2598f2f242afd2e8df61014b8eaa467d830
FernandoDurier <fernando.durier@hotmail.com>
2019-05-05 18:29:57 -0300

    refined the 1st deep clean logic

--- 1st-deep-clean.py
+++ 1st-deep-clean.py
@@ -19,39 +19,96 @@ for d in data:
     print('---------')
     refs=[]
     for r in d['references']:
+        reference={"refid":None,"title":None}
+        reference["refid"]=r["@id"]
+        temptitle=""
         
-        title = ''
-        if '#text' in r['mixed-citation']:
-            title+=(r['mixed-citation']['#text'])
-        if 'article-title' in r:
-            title+=(r['mixed-citation']['article-title'])
-
-        source = None
-        if 'source' in r['mixed-citation']:
-            source=r['mixed-citation']['source']
-        authors = ''
-        if 'string-name' in r['mixed-citation']:
-            author=''
+        if('string-name' in r['mixed-citation']):
             if type(r['mixed-citation']['string-name']) is list:
-                for a in r['mixed-citation']['string-name']:  
-                    if 'surname' in a:
-                        author+=(a['surname'])
-                    if 'given-name' in a:
-                        author+=(', '+a['given-name'])
-                    author+=(', ')
+                for sn in r['mixed-citation']['string-name']:
+                    temptitle = temptitle + sn['surname']
+                    if('#text' in sn):
+                        temptitle = temptitle + sn['#text']
+                    if('given-names' in sn):
+                        temptitle = temptitle + sn['given-names']
+                    temptitle = temptitle + ', '
             else:
-                if 'surname' in r['mixed-citation']['string-name']:
-                    author+=(r['mixed-citation']['string-name']['surname'])
-                if 'given-name' in r['mixed-citation']['string-name']:
-                    author+=(', '+r['mixed-citation']['string-name']['given-name'])
-                author+=(', ')
-            authors+=author+', '
-            
-        reference = {"authors":authors,
-        "title":title,
-        "source":source}
-        print(reference)
+                temptitle = temptitle + r['mixed-citation']['string-name']['surname']
+                if('#text' in r['mixed-citation']['string-name']):
+                    temptitle = temptitle + r['mixed-citation']['string-name']['#text']
+                if('given-names' in r['mixed-citation']['string-name']):
+                    temptitle = temptitle + r['mixed-citation']['string-name']['given-names']
+                temptitle = temptitle + ', '
+                
+
+        if('#text' in r['mixed-citation']):
+            temptitle = temptitle + r['mixed-citation']['#text'] 
+
+        if('year' in r['mixed-citation']):
+            if(type(r['mixed-citation']['year']) is list):
+                temptitle = temptitle + r['mixed-citation']['year'][0] + ', '
+            else:
+                temptitle = temptitle + r['mixed-citation']['year'] + ', '
+
+        if 'article-title' in r['mixed-citation']:
+            if type(r['mixed-citation']['article-title']) is list:
+                for at in r['mixed-citation']['article-title']:
+                    temptitle = temptitle + " " + at
+            else:
+                temptitle = temptitle + r['mixed-citation']['article-title']
+        temptitle = temptitle + ', '
+
+        if('source' in r['mixed-citation']):
+            if type(r['mixed-citation']['source']) is list:
+                for s in r['mixed-citation']['source']:
+                    temptitle = temptitle + s + ', '
+            else:    
+                temptitle = temptitle + r['mixed-citation']['source'] + ' '
+        
+        if('volume' in r['mixed-citation']):
+            if(type(r['mixed-citation']['volume']) is list):
+                for v in r['mixed-citation']['volume']:
+                    temptitle = temptitle + v + ', '
+            else:
+                temptitle = temptitle + r['mixed-citation']['volume'] + ' '
+        
+        if('issue' in r['mixed-citation']):
+            if(type(r['mixed-citation']['issue']) is list):
+                temptitle = temptitle + '('
+                for i in r['mixed-citation']['issue']:
+                    temptitle = temptitle + i + ', '
+                temptitle = temptitle + ')'
+            else:
+                temptitle = temptitle + '(' + r['mixed-citation']['issue'] + ') '
+        
+        if('fpage' in r['mixed-citation']):
+            if(type(r['mixed-citation']['fpage']) is list):
+                for f in r['mixed-citation']['fpage']:
+                    temptitle = temptitle + ', ' + f
+            else:
+                temptitle = temptitle + ', ' + r['mixed-citation']['fpage']
+        
+        if('lpage' in r['mixed-citation']):
+            if(type(r['mixed-citation']['lpage']) is list):
+                for l in r['mixed-citation']['lpage']:
+                    temptitle = temptitle + ', ' + l
+            else:
+                temptitle = temptitle + '-' + r['mixed-citation']['lpage']
+
+        temptitle = temptitle.replace(', , ', ', ')
+        temptitle = temptitle.replace('\n ', ' ')
+        temptitle = temptitle.replace(' . ', ' ')
+        temptitle = temptitle.replace('      ', ' ')
+        temptitle = temptitle.replace('     ', ' ')
+        temptitle = temptitle.replace('    ', ' ')
+        temptitle = temptitle.replace('   ', ' ')
+        temptitle = temptitle.replace('  ', ' ')
+        temptitle = temptitle.strip()
+        temptitle = temptitle.lstrip()
+        temptitle = temptitle.rstrip()
+        reference['title'] = temptitle
         refs.append(reference)
+
     d['clean-refs']=refs
     
     authors=[]
